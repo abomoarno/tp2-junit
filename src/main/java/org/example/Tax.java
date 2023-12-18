f3c25bec438a994a2dcf72b00345663ef259c453
arnoabomo <abomoarno@gmail.com>
2023-12-18 21:32:01 +0100

    Commit TP2

--- src/main/java/org/example/Tax.java
+++ src/main/java/org/example/Tax.java
@@ -7,37 +7,4 @@ public class Tax {
     public Tax(Employe emp) {
         this.emp = emp;
     }
-
-    public double calculNet(){
-        return emp.getBrute()  - calculTax();
-    }
-
-    public double calculTax(){
-        return emp.getBrute() * getTaux();
-    }
-
-    private double getTaux(){
-        double taux;
-
-        if (emp.getBrute() >= 60000.0){
-            taux = .15;
-        } else if (emp.getBrute() >= 45000.0) {
-            taux = .1;
-        } else if (emp.getBrute() >= 3500){
-            taux = .075;
-        } else {
-            taux = 0.0;
-        }
-
-        double reduction;
-
-        if ((taux > 0.0) &&( emp.getEnfant() > 0)){
-            reduction = 0.025;
-        } else {
-            reduction = 0.0;
-        }
-
-        return taux - reduction;
-    }
-
 }
