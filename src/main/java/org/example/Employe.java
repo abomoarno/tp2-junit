8121a92ec8a8c80aa7fbeb31700f94db10fa0573
arnoabomo <abomoarno@gmail.com>
2023-12-18 21:27:35 +0100

    Commit TP2

--- src/main/java/org/example/Employe.java
+++ src/main/java/org/example/Employe.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class Employe {
-    String nom;
-    int age;
-    double brute;
-    int enfant;
+    private String nom;
+    private int age;
+    private double brute;
+    private int enfant;
 
     public Employe(String nom, int age, double brute, int enfant) {
         this.nom = nom;
@@ -21,5 +21,37 @@ public class Employe {
                 brute +
                 " - " +
                 enfant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getBrute() {
+        return brute;
+    }
+
+    public void setBrute(double brute) {
+        this.brute = brute;
+    }
+
+    public int getEnfant() {
+        return enfant;
+    }
+
+    public void setEnfant(int enfant) {
+        this.enfant = enfant;
     }
 }
--- src/main/java/org/example/Tax.java
+++ src/main/java/org/example/Tax.java
@@ -9,21 +9,21 @@ public class Tax {
     }
 
     public double calculNet(){
-        return emp.brute  - calculTax();
+        return emp.getBrute()  - calculTax();
     }
 
     public double calculTax(){
-        return emp.brute * getTaux();
+        return emp.getBrute() * getTaux();
     }
 
     private double getTaux(){
         double taux;
 
-        if (emp.brute >= 60000.0){
+        if (emp.getBrute() >= 60000.0){
             taux = .15;
-        } else if (emp.brute >= 45000.0) {
+        } else if (emp.getBrute() >= 45000.0) {
             taux = .1;
-        } else if (emp.brute >= 3500){
+        } else if (emp.getBrute() >= 3500){
             taux = .075;
         } else {
             taux = 0.0;
@@ -31,7 +31,7 @@ public class Tax {
 
         double reduction;
 
-        if ((taux > 0.0) &&( emp.enfant > 0)){
+        if ((taux > 0.0) &&( emp.getEnfant() > 0)){
             reduction = 0.025;
         } else {
             reduction = 0.0;
